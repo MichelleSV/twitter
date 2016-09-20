95bc7840e96e9ef91c6f3a6dff8ef1949902683a
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-09-20 14:51:33 -0500

    subiendo version 2

--- js/app.js
+++ js/app.js
@@ -2,17 +2,23 @@ window.addEventListener("load", function() {
 	var boton = document.getElementById("btn-agregar");
 	boton.addEventListener("click", function() {
 		var tweet = document.getElementById("tweet").value;
+		var contadorDeNumbers = document.getElementById("contadorNumeros");
+		document.getElementById("tweet").value = "";
+        contadorDeNumbers.textContent = "140";
 		newTweet(tweet);
 	});
 	function newTweet (tweet) {
         var nuevoTweet = document.createElement("div");
         var contenedor = document.getElementById("contenedor");
-        nuevoTweet.innerText = tweet;
-		 if(!contenedor.childNodes[0]){
+		nuevoTweet.innerText = tweet;
+		if(tweet == ""){
+			boton.disabled=true;
+		}
+		 else if(!contenedor.childNodes[0]){
             contenedor.appendChild(nuevoTweet);
         } else {
 			contenedor.insertBefore(nuevoTweet,contenedor.childNodes[0]);
-        }
+		}	
 	}
 	tweet.addEventListener("keydown",function(){
 		contador(tweet);
@@ -20,10 +26,22 @@ window.addEventListener("load", function() {
 	function contador(tweet){
 		boton.disabled=false;
 		var limite = 140;
+		var contadorDeNumbers=document.getElementById("contadorNumeros");
 		var longitud = document.getElementById("tweet").value.length;
-		document.getElementById("contadorNumeros").innerHTML= limite-longitud;
-		if(longitud=>limite){
+		var count = document.getElementById("contadorNumeros").innerHTML= limite-longitud;
+		if(longitud>=140){
+			contadorDeNumbers.style.color="red";
 			boton.disabled=true;
+		}else if(longitud>=120){
+			contadorDeNumbers.style.color="blue";
+			boton.disabled=false;
+		}
+		else if(longitud>=130){
+			contadorDeNumbers.style.color="green";
+			boton.disabled=true;
+		}
+		else{
+			contadorDeNumbers.style.color="black";
 		}
 	}
 });
